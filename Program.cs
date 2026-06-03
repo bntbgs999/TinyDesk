27abae99f6f18175d945984731fbaa575aac134f
bagasthedev <bintangbgass@gmail.com>
2026-06-04 02:31:58 +0700

    feat: add auto-elevation to bypass smartscreen block

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@
 // =================================================================
 
 using System;
+using System.Diagnostics;
+using System.Security.Principal;
 using System.Windows.Forms;
 
 namespace InputController
@@ -13,8 +15,39 @@ namespace InputController
         [STAThread]
         static void Main()
         {
+            if (!IsAdministrator())
+            {
+                // Restart program with administrator privileges
+                ProcessStartInfo proc = new ProcessStartInfo();
+                proc.UseShellExecute = true;
+                proc.WorkingDirectory = Environment.CurrentDirectory;
+                proc.FileName = Application.ExecutablePath;
+                proc.Verb = "runas";
+
+                try
+                {
+                    Process.Start(proc);
+                }
+                catch
+                {
+                    // The user refused the elevation
+                    return;
+                }
+                Application.Exit();
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.Run(new SplashForm());
+        }
+
+        private static bool IsAdministrator()
+        {
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
         }
     }
 }
